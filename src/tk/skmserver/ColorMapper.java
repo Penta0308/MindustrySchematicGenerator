164d5ed00bac83465a59672cd545db7f86fb3f1f
Penta0308 <38977908+Penta0308@users.noreply.github.com>
2020-02-24 19:44:15 +0900

    Initial commit

---
+++ src/tk/skmserver/ColorMapper.java
@@ -0,0 +1,91 @@
+package tk.skmserver;
+
+import java.awt.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class ColorMapper {
+    public static Map<Integer, Integer> colorspace;
+
+/*
+#d99d73 copper
+#8c7fa9 lead
+#ebeef5 metaglass
+#b2c6d2 graphite
+#f7cba4 sand
+#272727 coal
+#8da1e3 titanium
+#f9a3c7 thorium
+#777777 scrap
+#53565c silicon
+#cbd97f plastanium
+#f4ba6e phase-fabric
+#f3e979 surge-alloy
+#7457ce spore-pod
+#ff795e blast-compound
+#ffaa5f pyratite
+ */
+
+    public ColorMapper() {
+        colorspace = new HashMap<>();
+        /*colorspace.put(0xd99d73, "copper");
+        colorspace.put(0x8c7fa9, "lead");
+        colorspace.put(0xebeef5, "metaglass");
+        colorspace.put(0xb2c6d2, "graphite");
+        colorspace.put(0xf7cba4, "sand");
+        colorspace.put(0x272727, "coal");
+        colorspace.put(0x8da1e3, "titanium");
+        colorspace.put(0xf9a3c7, "thorium");
+        colorspace.put(0x777777, "scrap");
+        colorspace.put(0x53565c, "silicon");
+        colorspace.put(0xcbd97f, "plastaium");
+        colorspace.put(0xf4ba6e, "phase-fabric");
+        colorspace.put(0xf3e979, "surge-alloy");
+        colorspace.put(0x7457ce, "spore-pod");
+        colorspace.put(0xff795e, "blast-compound");
+        colorspace.put(0xffaa5f, "pyratite");
+        */
+        colorspace.put(0xd99d73, 0);
+        colorspace.put(0x8c7fa9, 1);
+        colorspace.put(0xebeef5, 2);
+        colorspace.put(0xb2c6d2, 3);
+        colorspace.put(0xf7cba4, 4);
+        colorspace.put(0x272727, 5);
+        colorspace.put(0x8da1e3, 6);
+        colorspace.put(0xf9a3c7, 7);
+        colorspace.put(0x777777, 8);
+        colorspace.put(0x53565c, 9);
+        colorspace.put(0xcbd97f, 10);
+        colorspace.put(0xf4ba6e, 11);
+        colorspace.put(0xf3e979, 12);
+        colorspace.put(0x7457ce, 13);
+        colorspace.put(0xff795e, 14);
+        colorspace.put(0xffaa5f, 15);
+    }
+
+
+    public Integer ColorMapper(Color c) {
+        int i = (c.getRed() << 16) & 0x00ff0000 | (c.getGreen() << 8) & 0x0000ff00 | c.getBlue() & 0x000000ff;
+        AtomicReference<Integer> nowLessy = new AtomicReference<>(0);
+        AtomicReference<Double> nowValue = new AtomicReference<>(Double.MAX_VALUE);
+        colorspace.forEach((k, v) -> {
+            double e = getLength(k, i);
+            if( e < nowValue.get()) {
+                nowValue.set(e);
+                nowLessy.set(v);
+            }
+        });
+        return nowLessy.get();
+    }
+
+    public double getLength(int i, int i1) {
+        int r1 = (i >> 16) & 0xFF;
+        int r2 = (i1>> 16) & 0xFF;
+        int g1 = (i >> 8) & 0xFF;
+        int g2 = (i1>> 8) & 0xFF;
+        int b1 = i & 0xFF;
+        int b2 = i1& 0xFF;
+        return Math.sqrt((r1-r2)^2 + (g1-g2)^2 + (b1-b2)^2);
+    }
+}
---
+++ src/tk/skmserver/Main.java
@@ -0,0 +1,180 @@
+package tk.skmserver;
+
+import arc.files.Fi;
+import arc.struct.*;
+import arc.util.io.Streams;
+import arc.util.serialization.Base64Coder;
+import mindustry.world.Pos;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.zip.DeflaterOutputStream;
+import java.util.zip.InflaterInputStream;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException { //ImagePath Resolution(x y) OutputPath
+        if (args.length == 6 && args[0].equals("generate")) {
+            File original_image = new File(args[1]);
+            BufferedImage buffer_original_image = null;
+
+            try {
+                buffer_original_image = ImageIO.read(original_image);
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.exit(1);
+            }
+            int width = Integer.parseInt(args[2]);
+            int height = Integer.parseInt(args[3]);
+
+            BufferedImage buffer_post_image = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
+            Graphics2D graphic = buffer_post_image.createGraphics();
+            graphic.drawImage(buffer_original_image, 0, 0, width, height, null);
+
+            ColorMapper colorMapper = new ColorMapper();
+
+            Array<TTile> tiles = new Array<>();
+
+            for (int y = 0; y < height; y++) {
+                for (int x = 0; x < width; x++) {
+                    tiles.add(new TTile(null, x, y, colorMapper.ColorMapper(new Color(buffer_post_image.getRGB(x, y))), (byte) 0));
+                }
+            }
+
+
+            StringMap tags = new StringMap();
+            //tags.put("name", args[4]);
+            Schematic sch = new Schematic(tiles, tags, (short) width, (short) height);
+
+            if (args[5].equals("STDIO")) {
+                Streams.OptimizedByteArrayOutputStream out = new Streams.OptimizedByteArrayOutputStream(1024);
+                out.reset();
+                write(sch, out);
+                System.out.print(new String(Base64Coder.encode(out.getBuffer(), out.size())));
+            } else {
+                write(sch, new Fi(args[5]).write(false, 1024));
+            }
+
+
+        } else if (args.length == 3 && args[0].equals("extract")) {
+            File fi = new File(args[1]);
+            FileInputStream fis = null;
+            try {
+                fis = new FileInputStream(fi);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            try {
+                fis.skip(5);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            InflaterInputStream is = new InflaterInputStream(fis);
+
+            if (args[2].equals("STDIO")) {
+                Streams.OptimizedByteArrayOutputStream out = new Streams.OptimizedByteArrayOutputStream(1024);
+                out.reset();
+                while(fis.available() != 0) {
+                    out.write(fis.read());
+                }
+
+                System.out.print(new String(Base64Coder.encode(out.getBuffer(), out.size())));
+            } else {
+                try {
+                    Files.copy(is, Paths.get(args[2]));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        } else {
+            System.out.println("Usage : generate <imagepath> <X-resolution> <Y-resolution> <name> <outputpath>");
+            System.out.println("Usage : extract <schpath> <outputpath>");
+            System.out.println("set <outputpath> \"STDIO\" to get output by base64");
+        }
+    }
+
+    public static class TBlock {
+        public String name;
+        public TBlock(String name) {
+            this.name = name;
+        }
+    }
+
+    public static class TTile {
+
+        public TBlock block;
+        public int config;
+        public byte rotation = 0;
+        public short x, y;
+
+        public TTile(TBlock block, int x, int y, int config, byte rotation) {
+            this.block = block;
+            this.x = (short)x;
+            this.y = (short)y;
+            this.config = config;
+            this.rotation = rotation;
+        }
+    }
+
+    public static class Schematic {
+        public short width;
+        public short height;
+        public StringMap tags;
+        public Array<TTile> tiles;
+
+        public Schematic(Array<TTile> tiles, StringMap tags, short width, short height) {
+            this.tiles = tiles;
+            this.tags = tags;
+            this.width = width;
+            this.height = height;
+        }
+    }
+
+    private static final byte[] header = {'m', 's', 'c', 'h'};
+    private static final byte version = 0;
+
+    public static void write(Schematic schematic, OutputStream output) throws IOException {
+        output.write(header);
+        output.write(version);
+
+        try (DataOutputStream stream = new DataOutputStream(new DeflaterOutputStream(output))) {
+
+            stream.writeShort(schematic.width);
+            stream.writeShort(schematic.height);
+
+            stream.writeByte(0);
+            //stream.writeByte(schematic.tags.size);
+            //for (ObjectMap.Entry<String, String> e : schematic.tags.entries()) {
+            //    stream.writeUTF(e.key);
+            //    stream.writeUTF(e.value);
+            //}
+
+            //OrderedSet<TBlock> blocks = new OrderedSet<>();
+            //schematic.tiles.each(t -> blocks.add(t.block));
+
+            //create dictionary
+            //stream.writeByte(blocks.size);
+            //for (int i = 0; i < blocks.size; i++) {
+            //    stream.writeUTF(blocks.orderedItems().get(i).name);
+            //}
+            stream.writeByte(1);
+            stream.writeUTF("sorter");
+
+            stream.writeInt(schematic.tiles.size);
+            //write each tile
+            for (TTile tile : schematic.tiles) {
+                //stream.writeByte(blocks.orderedItems().indexOf(tile.block));
+                stream.writeByte(0);
+                stream.writeInt(Pos.get(tile.x, tile.y));
+                stream.writeInt(tile.config);
+                stream.writeByte(tile.rotation);
+            }
+        }
+    }
+}
